43396b1e087045ff8fbd530963f532da7f655ff5
osman <osmanrustamovbk@gmail.com>
2023-03-23 17:29:55 +0500

    updated

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ for (let i = 2023; i >= 1900; i--) {
     let options = document.createElement("OPTION");
     document.getElementById("year").appendChild(options).innerHTML = i;
 }
+for (let i = 1; i < 32; i++) {
+    let options = document.createElement("OPTION");
+    document.getElementById("day").appendChild(options).innerHTML = i;
+}
+let months = ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь']
 let name = document.querySelector(".name").value;
 let group = document.querySelector(".group").value;
 let sex = document.querySelector(".sex").value;
@@ -13,6 +18,11 @@ let btn = document.getElementById("submit");
 let test = document.getElementById("year");
 let val = test.options[test.selectedIndex].value;
 let birth = birthday + '.' + birthmonth + '.' + birthyear
+
+for (let i = 0; i < 12; i++) {
+    let options = document.createElement("OPTION");
+    document.getElementById("month").appendChild(options).innerHTML = months[i];
+}
 
 btn.onclick = function () {
     if (name != "" & group != "" & sex != "" & birthday != "" & birthmonth != "" & birthyear != "") {
