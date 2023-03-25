4e600432de1498aad8288a2e7a0525da698c0916
osman <osmanrustamovbk@gmail.com>
2023-03-25 15:28:05 +0500

    fix day in februare

--- src/works/task3/js/script.js
+++ src/works/task3/js/script.js
@@ -7,41 +7,20 @@ let birthmonth = document.querySelector(".birthmonth").value;
 let birthyear = document.querySelector(".birthyear").value;
 let isDriver = document.querySelector(".isDriver").value;
 let btn = document.getElementById("submit");
-let test = document.getElementById("year");
-let val = test.options[test.selectedIndex].value;
 let birth = birthday + '.' + birthmonth + '.' + birthyear
-
-
-function setYears()
-{
-    for (let i = 2023; i >= 1900; i--)
-    {
-        let options = document.createElement("OPTION");
-        document.getElementById("year").appendChild(options).innerHTML = i;
-    }
-}
-
-function getYear()
-{
-    let today = new Date()
-    let yearBeforePension = today.getUTCFullYear() - birthyear
-    return yearBeforePension
-}
-
-function setMonths()
-{
-    for (let i = 0; i < 12; i++)
-    {
-        let options = document.createElement("OPTION");
-        document.getElementById("month").appendChild(options).innerHTML = months[i];
-    }
-}
 
 function setDay()
 {
-    if (birthmonth === "Февраль")
+    if (birthmonth === "Февраль" & birthyear % 4 == 0 & birthyear != 1900)
     {
         for (let i = 1; i < 29; i++)
+        {
+            let options = document.createElement("OPTION");
+            document.getElementById("day").appendChild(options).innerHTML = i;
+        }
+    } else if (birthmonth === "Февраль" & birthyear % 4 != 0 & birthyear == 1900)
+    {
+        for (let i = 1; i < 28; i++)
         {
             let options = document.createElement("OPTION");
             document.getElementById("day").appendChild(options).innerHTML = i;
@@ -61,7 +40,39 @@ function setDay()
             document.getElementById("day").appendChild(options).innerHTML = i;
         }
     }
+}
 
+function setMonths()
+{
+    for (let i = 0; i < 12; i++)
+    {
+        let options = document.createElement("OPTION");
+        document.getElementById("month").appendChild(options).innerHTML = months[i];
+    }
+}
+
+function setYears()
+{
+    for (let i = 2023; i >= 1900; i--)
+    {
+        let options = document.createElement("OPTION");
+        document.getElementById("year").appendChild(options).innerHTML = i;
+    }
+}
+
+let day = document.getElementById("day")
+let month = document.getElementById("month")
+let year = document.getElementById("year")
+
+day = day.options[day.selectedIndex].value
+month = month.options[month.selectedIndex].value
+year = year.options[year.selectedIndex].value
+
+function getYear()
+{
+    let today = new Date()
+    let yearBeforePension = today.getUTCFullYear() - birthyear
+    return yearBeforePension
 }
 
 
