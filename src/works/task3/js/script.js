494d1f6a8f20330e38cb45ea3edcde68bc7ef891
osman <osmanrustamovbk@gmail.com>
2023-03-31 10:03:40 +0500

    fix js

--- src/works/task3/js/script.js
+++ src/works/task3/js/script.js
@@ -10,11 +10,13 @@ let btn = document.getElementById("submit");
 
 function setDay()
 {
+    let daySelect = document.getElementById("day")
     if (birthmonth === "Февраль" & birthyear % 4 == 0 & birthyear != 1900)
     {
         for (let i = 1; i < 29; i++)
         {
             let options = document.createElement("OPTION");
+            options.value = i
             document.getElementById("day").appendChild(options).innerHTML = i;
         }
     } else if (birthmonth === "Февраль" & birthyear % 4 != 0 & birthyear == 1900)
@@ -22,6 +24,7 @@ function setDay()
         for (let i = 1; i < 28; i++)
         {
             let options = document.createElement("OPTION");
+            options.value = i
             document.getElementById("day").appendChild(options).innerHTML = i;
         }
     } else if (birthmonth === "Апрель" || birthmonth === "Июнь" || birthmonth === "Сентябрь" || birthmonth === "Ноябрь")
@@ -29,6 +32,7 @@ function setDay()
         for (let i = 1; i < 31; i++)
         {
             let options = document.createElement("OPTION");
+            options.value = i
             document.getElementById("day").appendChild(options).innerHTML = i;
         }
     } else
@@ -36,26 +40,58 @@ function setDay()
         for (let i = 1; i < 32; i++)
         {
             let options = document.createElement("OPTION");
+            options.value = i
             document.getElementById("day").appendChild(options).innerHTML = i;
         }
+    }
+    daySelect.addEventListener("change", function()
+    {
+        localStorage.setItem("selectedDay", daySelect.value);
+    });
+
+    let savedDay = localStorage.getItem("selectedDay");
+    if (savedDay) {
+        daySelect.value = savedDay;
     }
 }
 
 function setMonths()
 {
+    let monthSelect = document.getElementById("month")
     for (let i = 0; i < 12; i++)
     {
         let options = document.createElement("OPTION");
+        options.value = months[i]
         document.getElementById("month").appendChild(options).innerHTML = months[i];
+    }
+    monthSelect.addEventListener("change", function()
+    {
+        localStorage.setItem("selectedMonth", monthSelect.value);
+    })
+    let savedMonth = localStorage.getItem("selectedMonth");
+    if (savedMonth) {
+        monthSelect.value = savedMonth;
     }
 }
 
 function setYears()
 {
+    let yearSelect = document.getElementById("year");
     for (let i = 2023; i >= 1900; i--)
     {
         let options = document.createElement("OPTION");
-        document.getElementById("year").appendChild(options).innerHTML = i;
+        options.innerHTML = i;
+        yearSelect.appendChild(options);
+    }
+
+    yearSelect.addEventListener("change", function()
+    {
+        localStorage.setItem("selectedYear", yearSelect.value);
+    });
+
+    let savedYear = localStorage.getItem("selectedYear");
+    if (savedYear) {
+        yearSelect.value = savedYear;
     }
 }
 
